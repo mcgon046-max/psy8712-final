42dd9411846e9fab5456f68a1251ea604da16e60
Patrick Mcgonagle <mcgon046@umn.edu>
2026-05-06 17:21:01 -0500

    Commiting image

--- R/final.R
+++ R/final.R
@@ -38,7 +38,7 @@ corpus_prep <- corpus |>
   tm_map(content_transformer(str_to_lower)) |> # Makes everything lowercase 
   tm_map(removeNumbers) |> # Gets rid of numbers 
   tm_map(removePunctuation) |> # Gets rid of punctuation 
-  tm_map(content_transformer(lemmatize_words)) |> # Lemmatizing for text stems 
+  tm_map(content_transformer(lemmatize_strings)) |> # Lemmatizing for text stems 
   tm_map(removeWords, stopwords("en")) |> # Gets rid of stopwords 
   tm_map(stripWhitespace) # Gets rid of the whitespace from the lemmetization and the stopword removal 
 
@@ -56,9 +56,49 @@ DTM <- DocumentTermMatrix(
   )
 
 ## Removing sparse terms 
-slimmed_dtm <- removeSparseTerms(DTM, .95) # set the threshhold to .90 meaning terms must appear in 95% of docs 
+slimmed_dtm <- removeSparseTerms(DTM, .95) # set the threshold to .95 meaning term is allowed to be empty in 95% of text 
 
 ## Converting back to dataframe 
+tokens_df <- as.matrix(slimmed_dtm) |> # Turns this into a matrix 
+  as.tibble() |> # Makes it a tibble for ML tasks, chosen as opposed to base R df due to tidy models
+  rename_with(make.names) # Naming cols so that tidy models work 
+
+## Final ML token df 
+ml_data_tokens <- bind_cols(
+  overall_rating = df_clean$overall_rating, 
+  tokens_df
+)
+
+## topic analysis 
+dfm2stm <- readCorpus(slimmed_dtm, type = "slam") # Read corpus to reformat the dtm for the k search
+
+## K-search algo 
+# kresult <- searchK(
+#   documents = dfm2stm$documents,
+#   vocab = dfm2stm$vocab,
+#   K = seq(2, 20, by = 2), # Tests K values from 2 - 20 in 2 topic increments, up to 20 
+#   verbose = TRUE # Needed to check on progress 
+# )
+ ### Commented out so it doesn't rerun on source click 
+
+
+
+## saving kresult plot to viz 
+png("figs/kfig.png", width = 16, height = 9, units = "in", res = 300)
+
+#plot
+plot(kresult)
+
+# Turn the device off to finalize the file save
+dev.off()
+
+### Note: I had to use base R not ggsave here to get the png in order for it save correctly 
+
+### It Appears that 6 topics are the best option here. 
+
+
+
+
 
 
 
