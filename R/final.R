b51d6f2823deb5f8317a4ff92cb2d4ed09a677a9
Patrick Mcgonagle <mcgon046@umn.edu>
2026-05-05 19:28:17 -0500

    save

--- R/final.R
+++ R/final.R
@@ -1,9 +1,26 @@
 # Script Settings and Resources 
 library(tidyverse)
 library(rollama) # Best package for API calls to Ollama apparently 
-
+library(caret) # ML tasks 
+library(tm) # Robust text handling w/ built in corpus handling 
+library(textstem) # Lemmatizatoin
+library(RWeka) # n-gram tokenization 
+library(stm)
 
 # Data Import and Cleaning 
 
-import <- read_csv("data/glassdoor_reviews.csv")
+df_import <- read_csv("data/glassdoor_reviews.csv") # Initial import read_csv as it creates a tibble for tidyverse, and is better to work with 
+
+df_clean <- df_import |>
+  select(
+    headline,
+    overall_rating, 
+    pros,
+    cons
+  ) |> # selecting text columns and ratings, 
+  drop_na() |> # dropping NAs 
+  mutate(
+    review_text = paste(headline, pros, cons, sep = " ")
+  ) # Mutate to put all the review text into a single string for NLP 
+
 
